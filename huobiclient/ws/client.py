6ba44c9eef8e124650180b69ad42efc0ff1f56b3
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-11-24 01:23:31 +0300

    Additional parameters `loads` and `decompress` in websocket classes

--- huobiclient/ws/client.py
+++ huobiclient/ws/client.py
@@ -1,7 +1,7 @@
 import asyncio
 import gzip
 import json
-from typing import AsyncGenerator, Dict, Type
+from typing import Any, AsyncGenerator, Callable, Dict, Type, Union
 
 from huobiclient.auth import WebsocketAuth
 from huobiclient.cfg import HUOBI_ACCESS_KEY, HUOBI_SECRET_KEY, HUOBI_WS_ASSET_AND_ORDER_URL, HUOBI_WS_MARKET_URL
@@ -9,38 +9,44 @@ from huobiclient.exceptions import WSHuobiError
 from huobiclient.ws.connection import WebsocketConnection
 from huobiclient.ws.subscribers.market import BaseMarketStream
 
+LOADS_TYPE = Callable[[Union[str, bytes]], Any]
+
 
 class HuobiMarketWebsocket:
 
     def __init__(
         self,
         url: str = HUOBI_WS_MARKET_URL,
+        loads: LOADS_TYPE = json.loads,
+        decompress: Callable[[bytes], bytes] = gzip.decompress,
         connection: Type[WebsocketConnection] = WebsocketConnection,
         **connection_kwargs,
     ):
-        self.connection = connection(url=url, **connection_kwargs)
+        self._loads = loads
+        self._decompress = decompress
+        self._connection = connection(url=url, **connection_kwargs)
 
     async def __aenter__(self):
-        await self.connection.connect()
+        await self._connection.connect()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.connection.close()
+        await self._connection.close()
 
     async def _pong(self, timestamp: int) -> None:
-        await self.connection.send({'pong': timestamp})
+        await self._connection.send({'pong': timestamp})
 
     async def subscribe(self, stream: BaseMarketStream):
         for message in stream.subscribe():
-            await self.connection.send(message)
+            await self._connection.send(message)
 
     async def unsubscribe(self, stream: BaseMarketStream):
         for message in stream.unsubscribe():
-            await self.connection.send(message)
+            await self._connection.send(message)
 
     async def __aiter__(self) -> AsyncGenerator[Dict, None]:
-        async for message in self.connection:
-            data = json.loads(gzip.decompress(message))
+        async for message in self._connection:
+            data = self._loads(self._decompress(message))
             if 'ping' in data:
                 asyncio.create_task(self._pong(data['ping']))
                 continue
@@ -54,10 +60,12 @@ class HuobiAccountOrderWebsocket:
         url: str = HUOBI_WS_ASSET_AND_ORDER_URL,
         access_key: str = HUOBI_ACCESS_KEY,
         secret_key: str = HUOBI_SECRET_KEY,
+        loads: LOADS_TYPE = json.loads,
         connection: Type[WebsocketConnection] = WebsocketConnection,
         **connection_kwargs,
     ):
         self._url = url
+        self._loads = loads
         self._access_key = access_key
         self._secret_key = secret_key
         self._connection = connection(url=url, **connection_kwargs)
@@ -102,7 +110,7 @@ class HuobiAccountOrderWebsocket:
 
     async def __aiter__(self) -> AsyncGenerator[Dict, None]:
         async for message in self._connection:
-            data = json.loads(message)
+            data = self._loads(message)
             if data.get('action', '') == 'ping':
                 asyncio.create_task(self._pong(data['data']['ts']))
                 continue
