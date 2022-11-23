b197c642994453870b2d0257b48c62c0b086e0de
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-11-23 18:34:36 +0300

    Websocket connection in separated file

--- huobiclient/ws/client.py
+++ huobiclient/ws/client.py
@@ -1,55 +1,13 @@
 import asyncio
 import gzip
 import json
-from typing import Any, AsyncGenerator, Dict, Optional, Type
-
-import aiohttp
-from aiohttp import ClientWebSocketResponse
+from typing import AsyncGenerator, Dict, Type
 
 from huobiclient.auth import WebsocketAuth
 from huobiclient.cfg import HUOBI_ACCESS_KEY, HUOBI_SECRET_KEY, HUOBI_WS_ASSET_AND_ORDER_URL, HUOBI_WS_MARKET_URL
 from huobiclient.exceptions import WSHuobiError
+from huobiclient.ws.connection import WebsocketConnection
 from huobiclient.ws.subscribers.market import BaseMarketStream
-
-
-class WebsocketConnection:
-
-    def __init__(
-        self,
-        url: str,
-        session: Type[aiohttp.ClientSession] = aiohttp.ClientSession,
-        **session_kwargs,
-    ):
-        self._url = url
-        if session_kwargs.get('connector') is None:
-            session_kwargs['connector'] = aiohttp.TCPConnector(ssl=False)
-        self._session = session(**session_kwargs)
-        self._socket: Optional[ClientWebSocketResponse] = None
-
-    async def close(self) -> None:
-        await self._session.close()
-        if self._socket is not None:
-            await self._socket.close()
-            self._socket = None
-
-    async def connect(self, **kwargs) -> None:
-        self._socket = await self._session.ws_connect(url=self._url, **kwargs)
-
-    async def receive(self) -> Dict:
-        if self._socket is None:
-            raise RuntimeError('Web socket is not connected')
-        return await self._socket.receive_json()
-
-    async def send(self, message: Dict) -> None:
-        if self._socket is None:
-            await self.connect()
-        await self._socket.send_json(message)
-
-    async def __aiter__(self) -> AsyncGenerator[Any, None]:
-        if self._socket is None:
-            raise RuntimeError('Web socket is not connected')
-        async for message in self._socket:
-            yield message.data  # type:ignore
 
 
 class HuobiMarketWebsocket:
---
+++ huobiclient/ws/connection.py
@@ -0,0 +1,44 @@
+from typing import Any, AsyncGenerator, Dict, Optional, Type
+
+import aiohttp
+from aiohttp import ClientWebSocketResponse
+
+
+class WebsocketConnection:
+
+    def __init__(
+        self,
+        url: str,
+        session: Type[aiohttp.ClientSession] = aiohttp.ClientSession,
+        **session_kwargs,
+    ):
+        self._url = url
+        if session_kwargs.get('connector') is None:
+            session_kwargs['connector'] = aiohttp.TCPConnector(ssl=False)
+        self._session = session(**session_kwargs)
+        self._socket: Optional[ClientWebSocketResponse] = None
+
+    async def close(self) -> None:
+        await self._session.close()
+        if self._socket is not None:
+            await self._socket.close()
+            self._socket = None
+
+    async def connect(self, **kwargs) -> None:
+        self._socket = await self._session.ws_connect(url=self._url, **kwargs)
+
+    async def receive(self) -> Dict:
+        if self._socket is None:
+            raise RuntimeError('Web socket is not connected')
+        return await self._socket.receive_json()
+
+    async def send(self, message: Dict) -> None:
+        if self._socket is None:
+            await self.connect()
+        await self._socket.send_json(message)
+
+    async def __aiter__(self) -> AsyncGenerator[Any, None]:
+        if self._socket is None:
+            raise RuntimeError('Web socket is not connected')
+        async for message in self._socket:
+            yield message.data  # type:ignore
