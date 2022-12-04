1a066a2107418be9c71232f68cd7bb84df27e0db
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-12-04 20:40:34 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup
+
+requirements = [
+]
+
+
+setup(
+    name='asynchuobi',
+    version='0.0.1',
+    url='https://github.com/sometastycake/asynchuobi',
+    license='MIT',
+    author='Mike M',
+    author_email='stopthisworldplease@outlook.com',
+    description='Unofficial asynchronous python client for Huobi cryptoexchange',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=['tests']),
+    zip_safe=False,
+    python_requires='>=3.7',
+    install_requires=requirements,
+    setup_requires=requirements,
+    include_package_data=True,
+)
